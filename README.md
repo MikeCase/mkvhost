31d9db6e9afaf0d65580c236bc16fdca4a37e040
Mike <michael.e.case@gmail.com>
2018-10-13 14:23:41 -0500

    updates

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-mkvhost
+### What is mkvhost
+
+mkvhost is an app to simplify the process of creating a vhost on a server. 
+
+### Usage
+
+mkvhost [server-root] [vhost.server.com] 
+
+This will create a basic server using fpm to serve php files. 
+
+mkvhost 
+
--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='mk-vhost',
-    version='1.0',
+    version='1.01',
     py_modules=['mkvhost'],
     include_package_data=True,
     install_requires=[
         'click',
-        'CloudFlare',
         'requests',
+        'python-cloudflare',
+        'pystache'
     ],
     entry_points='''
         [console_scripts]
