8ba570c6d6f11f2e0ecd3bca403125ddbeeca686
Mike <michael.e.case@gmail.com>
2020-02-16 06:44:03 -0600

    Found old code I never commited

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 ### What is mkvhost
 
-mkvhost is an app to simplify the process of creating a vhost on a server. 
+mkvhost is an app to simplify the process of creating a vhost on a server.
+Will also connect to cloudflare and add a subdomain.  
 
 ### Usage
 
-mkvhost [server-root] [vhost.server.com] 
+mkvhost -r [server-root] --host [vhost.domain.com]
+mkvhost -r [server-root] -p y --host [vhost.domain.com] 
 
 This will create a basic server using fpm to serve php files. 
 
-mkvhost 
+EX:
+sudo mkvhost -r /path/to/server/root --host example.domain.com -p y
 
--- mkvhost.py
+++ mkvhost.py
@@ -17,26 +17,28 @@ else:
     username = os.environ['SUDO_USER']
     userhome = os.path.expanduser(f"~{username}")
 
+
 def my_ip():
     url = 'https://api.ipify.org'
-    
+
     try:
         ip_address = requests.get(url).text
     except:
-        exit('%s: failed' % (url))
+        exit(f'{url}: failed')
     if ip_address == '':
-        exit('%s: failed' % (url))
+        exit(f'{url}: failed')
     if ':' in ip_address:
         ip_address_type = 'AAAA'
     else:
         ip_address_type = 'A'
-    
+
     return ip_address, ip_address_type
+
 
 def mkcfg(root, host, template, fpm):
 
     if os.path.isdir(f"{userhome}/vhosts/{host}/") is False:
-            os.makedirs(f"{userhome}/vhosts/{host}/log/")
+        os.makedirs(f"{userhome}/vhosts/{host}/log/")
 
     alog = f"{userhome}/vhosts/{host}/log/access_log"
     elog = f"{userhome}/vhosts/{host}/log/error_log"
@@ -52,20 +54,20 @@ def mkcfg(root, host, template, fpm):
         with open("index.tmpl", 'r') as tmpl:
             fp.write(render(tmpl.read(), locals()))
 
-    
+
 def mkdns(cf, host_name, zone_name, zone_id):
     host = f"{host_name}.{zone_name}"
     os.symlink(f"{confavail}/{host}", f"{confenabled}/{host}")
 
     ip_address, ip_address_type = my_ip()
-    
+
     dns_name = f"{host_name}.{zone_name}"
 
     dns_records = [
         {
-            'type':ip_address_type,
-            'name':host_name,
-            'content':ip_address,
+            'type': ip_address_type,
+            'name': host_name,
+            'content': ip_address,
             'proxied': True
         }
     ]
@@ -73,24 +75,24 @@ def mkdns(cf, host_name, zone_name, zone_id):
     try:
         for dns_record in dns_records:
             r = cf.zones.dns_records.post(zone_id, data=dns_record)
-        
-    except CloudFlare.exceptions.CloudFlareAPIError as e:
-        exit('/zones.dns_records.post %s - %d %s - api call failed' % (dns_name, e, e))
 
-    print('CREATED: %s %s' % (dns_name, ip_address))
+    except CloudFlare.exceptions.CloudFlareAPIError as e:
+        exit(f'/zones.dns_records.post {dns_name} - {e} {e} - api call failed')
+
+    print(f'CREATED: {dns_name} {ip_address}')
     subprocess.run(["service", "nginx", "reload"])
 
 
 def deldns(cf, host, zone_id):
-    
-    dns_records = cf.zones.dns_records.get(zone_id, params={'name':host})
+
+    dns_records = cf.zones.dns_records.get(zone_id, params={'name': host})
     for dns_record in dns_records:
         dns_record_id = dns_record['id']
         r = cf.zones.dns_records.delete(zone_id, dns_record_id)
-        
+
     delvhost(host)
 
-    
+
 def delvhost(host):
     confavailName = f"{confavail}/{host}"
     vdir = f"{userhome}/vhosts/{host}"
@@ -101,12 +103,12 @@ def delvhost(host):
 
     if os.path.exists(confavailName) and os.path.isfile(confavailName):
         os.remove(confavailName)
-    
+
     if os.path.exists(vdir) and os.path.isdir(vdir):
         shutil.rmtree(vdir)
-                    
+
     subprocess.run(["service", "nginx", "reload"])
-    
+
 
 @click.command()
 @click.option('--root', '-r', required=True, help="Set your root directory")
@@ -116,32 +118,35 @@ def delvhost(host):
 @click.option('--remove', '-d', is_flag=True, help="Remove Vhost")
 @click.option('--public', '-p', default=None, help="Would you like your vhost to be public?")
 def cli(root, host, template, fpm, remove, public):
+
+    cf = CloudFlare.CloudFlare(debug=True)
+
     """ 
     Setup cloudflare connection
     """
-    cf = CloudFlare.CloudFlare(debug=True)
 
     host_name, zone_name = host.split('.', 1)
     try:
-        params = {'name':zone_name}
+        params = {'name': zone_name}
         zones = cf.zones.get(params=params)
     except CloudFlare.exceptions.CloudFlareAPIError as e:
-        exit('/zones %d %s - api call failed' % (e, e))
+        exit(f'/zones {e} {e} - api call failed')
     except Exception as e:
-        exit('/zones.get - %s - api call failed' % (e))
+        exit(f'/zones.get - {e} - api call failed')
 
     if len(zones) == 0:
-        exit('/zones.get - %s - zone not found' % (zone_name))
-    
+        exit(f'/zones.get - {zone_name} - zone not found')
+
     if len(zones) != 1:
-        exit('/zones.get - %s - api call returned %d items' % (zone_name, len(zones)))
+        exit(
+            f'/zones.get - {zone_name} - api call returned {len(zones)} items')
 
     zone = zones[0]
     zone_name = zone['name']
     zone_id = zone['id']
 
     if remove is True:
-        print("Remove: True")
+        print("Removing DNS entry...")
         deldns(cf, host, zone_id)
     else:
         mkcfg(root, host, template, fpm)
