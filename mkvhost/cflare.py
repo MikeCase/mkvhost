6bd3840725d586311bb4b9522811d5619c34905c
Mike Case <michael.e.case@gmail.com>
2021-04-07 01:55:48 -0500

    Updated cflare.py added a private _build_api_path method to clean up code a bit.

--- mkvhost/cflare.py
+++ mkvhost/cflare.py
@@ -9,6 +9,7 @@ authentication. Maybe not tested extensively enough.
 
 '''
 import requests
+from urllib.parse import urlparse, urlunparse
 import json
 import sys
 import os
@@ -38,59 +39,92 @@ class CFlare:
         
         self.zone_id='c4f876ffa36284d86ce9145148994eee'
 
+
+
+
+    def _build_api_path(self, params, query):
+        """
+        params in this case are any parameters that the url will accept.
+        such as anything after zones. the zone id till you get to the query string
+
+        """
+        
+
+        self.url = urlparse(self.api_base_url)
+        self.params = params
+        self.query = query
+        self.api_path = ['client/v4']
+        if len(self.params) > 0:
+            for i in range(0,len(self.params)):
+                self.api_path.append(f'/{self.params[i]}')
+        
+        self.final_path = ''.join(self.api_path)
+        builturl = [
+            f'{self.url.scheme}',
+            f'{self.url.hostname}', 
+            f'{self.final_path}', 
+            f'{self.url.params}', 
+            f'{self.query}', 
+            f'{self.url.fragment}',
+        ]
+        prepped_url = urlunparse(builturl)
+
+        return prepped_url 
+
+        
+
     def show_zones(self):
-        q = f"?page=1&per_page=20"
-        url = ''.join([self.api_base_url, "/zones", f'{q}'])
+        q = f"page=1&per_page=20"
+        params = ['zones', self.zone_id]
+        url = self._build_api_path(params, q)
         resp = requests.request("GET", url, headers=self.headers)
         zones = resp.json()
-        # pprint.pprint(zones)
         resp.close()
         return zones
 
     def get_zone_id(self):
-        q = f"?name={self.zone_name}&page=1&per_page=20"
-        url = ''.join([self.api_base_url, "/zones", f'{q}'])
+        q = f"name={self.zone_name}&page=1&per_page=20"
+        params = ['zones']
+        url = self._build_api_path(params, q)
         resp = requests.request("GET", url, headers=self.headers)
         self.zone_id = resp.json()['result'][0]['id']
         resp.close()
         return self.zone_id
 
     def get_records(self):
-        q = "?page=1&per_page=20"
-        # print(self.zone_id)
-        url = ''.join([self.api_base_url, '/zones', f'/{self.zone_id}', '/dns_records', f'{q}'])
-        # print(url)
+        q = "page=1&per_page=20"
+        params = ['zones', self.zone_id, 'dns_records']
+        url = self._build_api_path(params, q)
         resp = requests.request("GET", url, headers=self.headers)
         zone_records = resp.json()
         resp.close()
-        # pprint.pprint(zone_records)
         return zone_records
 
     def add_record(self, payload, zid):
         q=''
-        url = ''.join([self.api_base_url, '/zones', f'/{zid}', '/dns_records'])
+        params = ['zones', zid, 'dns_records']
+        url = self._build_api_path(params, q)
         resp = requests.request("POST", url, headers=self.headers, data=payload)
-        # pprint.pprint(resp.json())
         result = resp.json()
         resp.close()
         return result
 
 
     def get_record_by_name(self, zone_id, record_name):
-        q=f'?name={record_name}'
-        url = ''.join([self.api_base_url, '/zones', f'/{zone_id}', '/dns_records', q])
+        q=f'name={record_name}'
+        params = ['zones', zone_id, 'dns_records']
+        url = self._build_api_path(params, q)
         resp = requests.request("GET", url, headers=self.headers)
-        # pprint.pprint(resp.json())
+        rec_name = resp.json()
         resp.close()
-        return resp.json()
+        return rec_name
     
     def del_record(self, zone_id, record_name):
         q=''
         q_id = self.get_record_by_name(zone_id, record_name)['result'][0]['id']
-        url = ''.join([self.api_base_url, '/zones', f'/{zone_id}', '/dns_records/', f'{q_id}'])
-        # print(url)
+        params = ['zones', zone_id, 'dns_records', q_id]
+        url = self._build_api_path(params, q)
         resp = requests.request("DELETE", url, headers=self.headers)
-        # print('delete_records')
-        # pprint.pprint(resp.json())
+        result = resp.json()
         resp.close()
-        return resp.json()
+        return result
--- tests/test_cflare_class.py
+++ tests/test_cflare_class.py
@@ -10,24 +10,21 @@ class TestCFlare(unittest.TestCase):
         self.record_id = ''
         self.payload = json.dumps({
                 "type": "A",
-                "name":f'testing',
+                "name": 'testing',
                 "content": "45.79.42.59",
                 "ttl": "1",
                 "proxied": True,
             })
 
     def test_get_zones(self):
-        # self.assertEquals(self.cf.show_zones(), dict())
-        self.assertEqual(self.cf.show_zones()['result'][0]['name'], f'mikecase.us')
+        self.assertEqual(self.cf.show_zones()['result']['name'], 'mikecase.us')
 
     def test_get_zone_id(self):
         self.assertEqual(self.cf.get_zone_id(), self.zone_id)
-        # self.assertEquals(CFlare)
 
     def test_get_dns_records(self):
         self.assertIsNotNone(self.cf.get_records())
         records = self.cf.get_records()
-        # print(records)
         self.assertEqual(records['success'], True)
 
     def test_add_dns_record(self):
@@ -35,9 +32,8 @@ class TestCFlare(unittest.TestCase):
         self.assertEqual(test['success'], True)
 
     def test_del_dns_record(self):
-        self.record_id = self.cf.get_record_by_name(self.zone_id, 'testing.mikecase.us')['result'][0]['name']
-        # print(self.record_id)
-        test = self.cf.del_record(self.zone_id, self.record_id)
+        record_name = self.cf.get_record_by_name(self.zone_id, 'testing.mikecase.us')['result'][0]['name']
+        test = self.cf.del_record(self.zone_id, record_name)
         self.assertEqual(test['success'], True)
 
     
