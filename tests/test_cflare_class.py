8966e13653500a60ee367552df0105340c9ca4b8
Mike Case <michael.e.case@gmail.com>
2021-04-07 10:52:12 -0500

    updates

--- tests/test_cflare_class.py
+++ tests/test_cflare_class.py
@@ -5,19 +5,19 @@ class TestCFlare(unittest.TestCase):
    
     # zone_name = 'mikecase.us'
     def setUp(self):
-        self.cf = CFlare('mikecase.us')
-        self.zone_id = 'c4f876ffa36284d86ce9145148994eee'
+        self.cf = CFlare('')
+        self.zone_id = ''
         self.record_id = ''
         self.payload = json.dumps({
                 "type": "A",
                 "name": 'testing',
-                "content": "45.79.42.59",
+                "content": "",
                 "ttl": "1",
                 "proxied": True,
             })
 
     def test_get_zones(self):
-        self.assertEqual(self.cf.show_zones()['result']['name'], 'mikecase.us')
+        self.assertEqual(self.cf.show_zones()['result']['name'], '')
 
     def test_get_zone_id(self):
         self.assertEqual(self.cf.get_zone_id(), self.zone_id)
@@ -32,7 +32,7 @@ class TestCFlare(unittest.TestCase):
         self.assertEqual(test['success'], True)
 
     def test_del_dns_record(self):
-        record_name = self.cf.get_record_by_name(self.zone_id, 'testing.mikecase.us')['result'][0]['name']
+        record_name = self.cf.get_record_by_name(self.zone_id, '')['result'][0]['name']
         test = self.cf.del_record(self.zone_id, record_name)
         self.assertEqual(test['success'], True)
 
