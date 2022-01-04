6a9bb81e712f4cac200b5e72c517353eeb192c4b
MikeCase <michael.e.case@gmail.com>
2022-01-04 14:54:11 -0600

    textvariable= isn't working.. wip

--- gui/settings_screen.py
+++ gui/settings_screen.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk, StringVar
+from tkinter import ttk
 import os
 import dotenv
 
@@ -12,9 +12,11 @@ class SettingsScreen(tk.Tk):
         self.title("Settings")
         self.geometry('400x300')
 
-        api_key = StringVar(value=os.getenv('token'))
-        api_email = StringVar(value=os.getenv('email'))
-        
+        api_key = tk.StringVar()
+        api_email = tk.StringVar()
+        api_key.set(value=os.getenv('token'))
+        api_email.set(value=os.getenv('email'))
+        print(api_email.get())
         lbl_frame = ttk.LabelFrame(self, text="Settings")
         lbl_frame.grid(row=0, column=0, padx=10, pady=10, sticky='nsew')
 
@@ -22,26 +24,26 @@ class SettingsScreen(tk.Tk):
         lbl_email = tk.Label(lbl_frame, text="Email Address")
 
 
-        txt_api_key = tk.Entry(lbl_frame, width=45)
-        txt_api_email = tk.Entry(lbl_frame, width=45)
-        txt_api_key.insert(0, api_key.get())
-        txt_api_email.insert(0, api_email.get())
-        txt_api_email.bind("<Return>", lambda: set_api_email(txt_api_email.get()))
-        txt_api_key.bind("<Return>", lambda: set_api_key(txt_api_key.get()))
+        txt_api_key = tk.Entry(lbl_frame, width=45, textvariable=api_key)
+        txt_api_email = tk.Entry(lbl_frame, width=45, textvariable=api_email)
+        txt_api_email.bind("<Return>", lambda x=txt_api_email.get(): api_email.set(value=x))
+        txt_api_key.bind("<Return>", lambda x=txt_api_key.get(): api_key.set(value=x))
         txt_api_email.grid(row=0, column=1, padx=5, pady=10)
         txt_api_key.grid(row=1, column=1, padx=5, pady=10)
         lbl_email.grid(row=0, column=0, padx=5, pady=10)
         lbl_api_key.grid(row=1, column=0, padx=5, pady=10)
 
-        btn_save = tk.Button(self, text="Save", command=lambda: self.save_api_info())
+        btn_save = tk.Button(self, text="Save", command=lambda x=api_email.get(), v=api_key.get(): self.save_api_info(x, v))
+        btn_save.grid(row=1, column=0)
 
-        def save_api_info(self):
-            with open('.env', 'w+') as env_file:
-                env_file.write(f"EMAIL={api_email.get()}")
-                env_file.write(f"TOKEN={api_key.get()}")
-                
-        def set_api_email(self, email):
-            api_email.set(value=email)
 
-        def set_api_key(self, token):
-            api_key.set(value=token)
+    def save_api_info(self, email, key):
+        with open('.env', 'w+') as env_file:
+            env_file.write(f"EMAIL = '{email}'\n")
+            env_file.write(f"TOKEN = '{key}'\n")
+            
+    def set_api_email(self, email):
+        self.api_email.set(value=email)
+
+    def set_api_key(self, token):
+        self.api_key.set(value=token)
